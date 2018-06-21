8e895f6d7285ac59a2aa26655030c35a602883e2
devoegesbot <39489458+devoegesbot@users.noreply.github.com>
2018-06-21 11:20:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var request			    = require('request');
 var bodyParser          = require('body-parser');
 var myPort= process.env.PORT || 3000;
 app.post('/botHandler',function(req,res){
+  console.log("////// LOGIN");
   console.log(req.body);
 res.send({
   "conversationToken": "{\"state\":null,\"data\":{}}",
