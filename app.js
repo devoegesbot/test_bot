d4efdad30339bc877ff6265cd49e533ed6023adf
devoegesbot <39489458+devoegesbot@users.noreply.github.com>
2018-06-21 11:05:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ var request			    = require('request');
 var bodyParser          = require('body-parser');
 var myPort= process.env.PORT || 3000;
 app.post('/botHandler',function(req,res){
-console.log("MY request body",req.body);
+  console.log(req.body);
+res.send({
+  "conversationToken": "{\"state\":null,\"data\":{}}",
+  "expectUserResponse": true,
+  "expectedInputs": [
+    {
+      "inputPrompt": {
+        "initialPrompts": [
+          {
+            "textToSpeech": "PLACEHOLDER_FOR_SIGN_IN"
+          }
+        ],
+        "noInputPrompts": []
+      },
+      "possibleIntents": [
+        {
+          "intent": "actions.intent.SIGN_IN",
+          "inputValueData": {}
+        }
+      ]
+    }
+  ]
+});
 })
 app.listen(myPort);
